54f516b0179fff8efa654bedc1b3442959fa43b2
Nisanth Kumar <nisanth.kumar@itraveller.com>
2019-10-16 21:59:18 +0530

    changes

--- resources/views/admin/editbooking.blade.php
+++ resources/views/admin/editbooking.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="container">
-        <h2>Edit User</h2>
+        <h2>Edit Booking</h2>
         <div class="table-responsive-sm">          
             <form method="post">
                 @foreach($user as $key => $data)
