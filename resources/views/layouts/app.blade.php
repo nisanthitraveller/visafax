ed7dc9c70bc14651ae6b47e9e0c7ab75d20d823b
Nisanth Kumar <nisanth.kumar@itraveller.com>
2019-11-01 13:36:22 +0530

    whatsapp number

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@ $requests = explode('/', Request::path());
             @if(!isset($requests[1]) || (isset($requests[1]) && $requests[1] != 'payment'))
                 @include('layouts.footer')
             @endif
-            <a target="_blank" href="https://api.whatsapp.com/send?phone=919074294969&text=Hello VisaBadge" class="chat"><img src="{{secure_url('images/whatsapp.png')}}" width="64" /></a>
+            <a target="_blank" href="https://api.whatsapp.com/send?phone={{env('PHONE_NUM')}}&text=Hello VisaBadge" class="chat"><img src="{{secure_url('images/whatsapp.png')}}" width="64" /></a>
         </div>
         
         <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
