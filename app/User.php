418fe8917b86a3c782b129d0b93bf19d8461e288
Nisanth Kumar <nisanth.kumar@itraveller.com>
2019-11-02 11:42:05 +0530

    changes

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'provider_id', 'avatar', 'token', 'provider'
+        'name', 'email', 'password', 'provider_id', 'avatar', 'token', 'provider', 'first_name', 'last_name', 'phone'
     ];
 
     /**
