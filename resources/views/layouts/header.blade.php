f3432b57e561bf49d0b69be8b41d3f416d694093
Nisanth Kumar <nisanth.kumar@itraveller.com>
2019-11-02 18:20:23 +0530

    changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -18,8 +18,8 @@
                                 <span class="user-title d-none d-sm-inline">{{ Auth::user()->name }}</span>
                             </button>
                             <div class="dropdown-menu">
-                                <a href="{{ route('my-visas') }}" class="dropdown-item my-visa">{{ __('My Visas') }}</a>
-                                <a class="dropdown-item" href="javascript: void(0)"
+                                <a href="{{ route('my-visas') }}" class="dropdown-item">{{ __('My Visas') }}</a>
+                                <a class="dropdown-item my-visa" href="javascript: void(0)"
                                    onclick="signOut(); event.preventDefault();
                                             document.getElementById('logout-form').submit();">
                                     {{ __('Sign out') }}
